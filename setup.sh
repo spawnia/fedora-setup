1b351b6bd5a46f030f23a915a2fe6bbc1bc889bd
spawnia <benedikt@franke.tech>
2020-06-07 13:01:05 +0200

    Automate dotfiles

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 git clone git@github.com:spawnia/dotfiles.git
-sudo dnf install stow
 cd dotfiles
 git submodule init
 git submodule update
+
+sudo dnf install stow
+
+rm ~/.bash_logout ~/.bash_profile ~/.bashrc
 stow bash
+
+rm ~/.gitconfig
 stow git
